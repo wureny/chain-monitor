e52ffaeae642c76ca0bfbbb6f0731c1a0dc166bd
wureny <wurenyu19@gmail.com>
2024-09-09 09:08:08 +0800

    feat: add flags

--- flags/flags.go
+++ flags/flags.go
@@ -2,26 +2,49 @@ package flags
 
 import "github.com/urfave/cli/v2"
 
+const prefix = "MONITOR"
+
+func prefixEnvVar(vari string) []string {
+	return []string{prefix + "_" + vari}
+}
+
 var (
 	RpcUrlFlag = &cli.StringFlag{
-		Name:        "",
+		Name:        "rpcUrl",
 		Category:    "",
 		DefaultText: "",
 		FilePath:    "",
 		Usage:       "",
-		Required:    false,
+		Required:    true,
 		Hidden:      false,
 		HasBeenSet:  false,
 		Value:       "",
 		Destination: nil,
 		Aliases:     nil,
-		EnvVars:     nil,
+		EnvVars:     prefixEnvVar("RPC_URL"),
 		TakesFile:   false,
 		Action:      nil,
 	}
 
 	MaxLenOfWatch = &cli.IntFlag{
-		Name:        "",
+		Name:        "maxLenOfWatch",
+		Category:    "",
+		DefaultText: "",
+		FilePath:    "",
+		Usage:       "",
+		Required:    true,
+		Hidden:      false,
+		HasBeenSet:  false,
+		Value:       0,
+		Destination: nil,
+		Aliases:     nil,
+		EnvVars:     prefixEnvVar("MAX_LEN_OF_WATCH"),
+		Base:        0,
+		Action:      nil,
+	}
+
+	MaxLenOfOperation = &cli.IntFlag{
+		Name:        "maxLenOfOperation",
 		Category:    "",
 		DefaultText: "",
 		FilePath:    "",
@@ -37,3 +60,9 @@ var (
 		Action:      nil,
 	}
 )
+
+func init() {
+
+}
+
+var Flags []cli.Flag
